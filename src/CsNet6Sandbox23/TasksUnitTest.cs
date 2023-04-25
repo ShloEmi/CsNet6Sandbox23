103815ec07fbaf2b76a7917957aac3865e800857
Barefoot <shlomi_o@hotmail.com>
2023-04-25 14:38:01 +0000

    WIP: +SemaphoreSlim usage +UnitsNet -need to rename tests 2+3

--- src/CsNet6Sandbox23/TasksUnitTest.cs
+++ src/CsNet6Sandbox23/TasksUnitTest.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using System.Diagnostics;
 
 namespace CsNet6Sandbox23;
 
@@ -6,15 +7,26 @@ public class TasksUnitTest
 {
     private int tId;
     private readonly SemaphoreSlim taskUnderTestSignal = new(0, 1);
+    private readonly SemaphoreSlim taskUnderTestSignals = new(0, 1);
+    TimeSpan dTwait;
 
-    public TasksUnitTest() => 
+    public TasksUnitTest()
+    {
         tId = -1;
 
+        dTwait = Debugger.IsAttached ?
+            TimeSpan.FromHours(1) :
+            TimeSpan.FromMilliseconds(100);
+    }
 
     async Task<int> TaskUnderTest()
     {
         tId = Environment.CurrentManagedThreadId;
         await taskUnderTestSignal.WaitAsync();
+        taskUnderTestSignals.Release();
+        ++tId;
+        await taskUnderTestSignal.WaitAsync();
+        taskUnderTestSignals.Release();
         return ++tId;
     }
 
@@ -41,9 +53,26 @@ public class TasksUnitTest
 
         // act
         taskUnderTestSignal.Release();
-        await task;
+        await taskUnderTestSignals.WaitAsync();
 
 
         tId.Should().Be(ctId + 1);
+    }
+
+    [Fact]
+    public async void TEST__startTaskUnderTest__RunHalfTask__ShouldPartialyRunTheTaskInSameThread3()
+    {
+        tId.Should().Be(-1);
+        int ctId = Environment.CurrentManagedThreadId;
+        Task<int> task = TaskUnderTest();
+
+
+        // act
+        taskUnderTestSignal.Release();
+        await taskUnderTestSignals.WaitAsync();
+        taskUnderTestSignal.Release();
+        task.Wait(dTwait).Should().BeTrue();
+
+        tId.Should().Be(ctId + 2);
     }
 }
