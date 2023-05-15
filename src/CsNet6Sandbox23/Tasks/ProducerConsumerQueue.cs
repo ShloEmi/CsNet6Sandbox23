e56ac8a3e343e210903127ae8d9ae17c421bc859
Barefoot <shlomi_o@hotmail.com>
2023-05-15 18:45:07 +0300

    WIP ProducerConsumerQueue

---
+++ src/CsNet6Sandbox23/Tasks/ProducerConsumerQueue.cs
@@ -0,0 +1,64 @@
+using FluentAssertions;
+using System.Collections.Concurrent;
+
+namespace CsNet6Sandbox23.ProducerConsumerQueue;
+
+public class ProducerConsumerQueueUnitTest
+{
+    /*
+    Write a C# program that simulates a producer-consumer scenario using a thread-safe queue. 
+    The program should create one or more producer threads that generate random numbers between 1 and 1000 and add them to the queue, 
+        and one or more consumer threads that remove numbers from the queue and calculate their square root. 
+    The program should terminate when the producers have generated 10,000 numbers and the consumers have processed them all. 
+    Use a CancellationToken to signal the threads to stop gracefully.
+    */
+
+
+    int produceWorkLeft = 10000;
+    int producerBatchSize = 101;
+    int producersTasksCount = 3;
+    CancellationTokenSource cts = new();
+
+
+    [Fact]
+    public async Task TEST__ProducerConsumerQueueAsync()
+    {
+        BlockingCollection<int> bc = new ();
+
+        Task[] producerTasks = new Task[producersTasksCount];
+
+        for (int i = 0; i < producersTasksCount; i++)
+        {
+            producerTasks[i] = ProducerTask(bc, cts.Token);
+        }
+    }
+
+
+    protected Task ProducerTask(BlockingCollection<int> bc, CancellationToken ct)
+    {
+        if (ct.IsCancellationRequested)
+            return Task.FromCanceled(ct);
+
+        var randomProvider = new Random();
+
+        bool running = true;
+        while (running)
+        {
+            if (ct.IsCancellationRequested)
+                return Task.FromCanceled(ct);
+
+
+            int workLeft = Interlocked.Add(ref produceWorkLeft, producerBatchSize);
+            if (workLeft + producerBatchSize <= 0 )
+                return Task.CompletedTask;
+
+            int workSize = workLeft + producerBatchSize;
+            for (; workSize > 0 ; workSize--)
+            {
+                bc.Add(randomProvider.Next(), ct);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/CsNet6Sandbox23/Tasks/SharedVariableIncrementationUnitTest.cs
@@ -0,0 +1,35 @@
+using FluentAssertions;
+
+namespace CsNet6Sandbox23.SharedVariableIncrementation;
+
+public class SharedVariableIncrementationUnitTest
+{
+    /*
+        Write a C# program that creates two threads and uses a shared variable to increment it in both threads simultaneously. 
+        Use the Interlocked class to perform atomic increments of the shared variable. 
+        Print the final value of the shared variable after both threads have finished executing.     
+    */
+
+    int sharedCount;
+
+
+    [Fact]
+    public void TEST__SharedVariableIncrementation()
+    {
+        CancellationTokenSource tcs = new();
+        Task[] tasks = new Task[2] ;
+
+        for (int i = 0; i < 2; i++)
+        {
+            tasks[i] = Task.Run(async () =>
+            {
+                await Task.Delay(100);
+                Interlocked.Increment(ref sharedCount);
+            }, tcs.Token);
+        }
+
+        Task.WaitAll(tasks, tcs.Token);
+
+        sharedCount.Should().Be(2);
+    }
+}
