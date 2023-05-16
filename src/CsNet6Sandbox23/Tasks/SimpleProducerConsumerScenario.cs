fb59cce5747e9b85de154ca3bc09cf96580b50fc
Barefoot <shlomi_o@hotmail.com>
2023-05-16 09:29:27 +0300

    +SimpleProducerConsumerScenario skeleton

---
+++ src/CsNet6Sandbox23/Tasks/SimpleProducerConsumerScenario.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+
+namespace CsNet6Sandbox23.SimpleProducerConsumerScenario;
+
+public class SimpleProducerConsumerScenarioUnitTest
+{
+    /*
+    Write a C# program that uses a Monitor object to implement a simple producer-consumer scenario. 
+    The program should use a thread-safe queue to hold the data, 
+     with one thread adding items to the queue (the producer) and another thread removing items from the queue (the consumer). 
+    The producer should wait when the queue is full, and the consumer should wait when the queue is empty. 
+    Use the Monitor object to ensure that only one thread accesses the queue at a time.
+    */
+
+
+    private Task SimpleProducerConsumerScenarioAsync()
+    {
+    }
+
+
+    [Fact]
+    public void TEST__SimpleProducerConsumerScenario()
+    {
+    }
+}
