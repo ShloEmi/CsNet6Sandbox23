a0ac51ebe6c0922cd874000ee8607f7552dfb0aa
Barefoot <shlomi_o@hotmail.com>
2023-05-16 10:38:15 +0300

    SimpleProducerConsumerScenario pre CR

--- src/CsNet6Sandbox23/Tasks/SimpleProducerConsumerScenario.cs
+++ src/CsNet6Sandbox23/Tasks/SimpleProducerConsumerScenario.cs
@@ -1,4 +1,4 @@
-using FluentAssertions;
+using Xunit.Abstractions;
 
 namespace CsNet6Sandbox23.SimpleProducerConsumerScenario;
 
@@ -12,14 +12,91 @@ public class SimpleProducerConsumerScenarioUnitTest
     Use the Monitor object to ensure that only one thread accesses the queue at a time.
     */
 
+    static readonly int queueLimit = 100;
+    Queue<int> queue = new(queueLimit);
+    object queueLock = new();
 
-    private Task SimpleProducerConsumerScenarioAsync()
+    bool running = true;
+    readonly AutoResetEvent queueFullSignal = new(false);
+    readonly AutoResetEvent queueEmptySignal = new(false);
+    readonly ManualResetEvent stopRunningSignal = new(false);
+
+    Random randomProvider = new();
+
+    private readonly ITestOutputHelper output;
+
+
+    public SimpleProducerConsumerScenarioUnitTest(ITestOutputHelper output)
     {
+        this.output = output;
+    }
+
+    protected Task ProducerTask()
+    {
+        return new Task( () => {
+            output.WriteLine("ProducerTask started");
+            while (running) { 
+                lock (queueLock)
+                {
+                    while (queue.Count < queueLimit)
+                        queue.Enqueue(randomProvider.Next());
+                }
+
+                queueFullSignal.Set();
+                WaitHandle.WaitAny(new WaitHandle[] { 
+                    stopRunningSignal, 
+                    queueEmptySignal 
+                    });
+            }
+            output.WriteLine("ProducerTask ended");
+        }, TaskCreationOptions.LongRunning);
+    }
+
+    protected Task ConsumerTask()
+    {
+        return new Task(() => {
+            output.WriteLine("ConsumerTask started");
+            while (running)
+            {
+                lock (queueLock)
+                {
+                    while (queue.Count > 0) 
+                    {
+                        int item = queue.Dequeue();
+                    }
+                }
+
+                queueEmptySignal.Set();
+                WaitHandle.WaitAny(new WaitHandle[] {
+                    stopRunningSignal,
+                    queueFullSignal
+                    });
+            }
+            output.WriteLine("ConsumerTask ended");
+        }, TaskCreationOptions.LongRunning);
     }
 
 
     [Fact]
-    public void TEST__SimpleProducerConsumerScenario()
+    public async Task TEST__SimpleProducerConsumerScenario()
     {
+        output.WriteLine("main start");
+
+        var producerTask = ProducerTask();
+        var consumerTask = ConsumerTask();
+
+        producerTask.Start();
+        consumerTask.Start();
+
+        output.WriteLine("tasks started");
+
+        await Task.Delay(250);
+
+        running = false;
+        stopRunningSignal.Set();
+        output.WriteLine("running = false");
+
+        Task.WaitAll(producerTask, consumerTask);
+        output.WriteLine("main end");
     }
 }
